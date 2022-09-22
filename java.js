201a3ac3e6206bb6cc4fc72b1fc660e0ba20f2d0
DavidJC1105 <112865062+DavidJC1105@users.noreply.github.com>
2022-09-22 11:54:43 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,136 @@
+console.log("Hello world") //This will print hello to console
+// Single line comment
+/*
+Multi line comment
+This comment will cover
+several lines
+*/
+//Variables
+
+/*
+There are two ways to declare variables in javascript
+
+var a = 10;
+
+let a = 10;
+
+const x = 20;
+
+*/
+
+let a =10;
+console.log(a);
+
+const x =20;
+console.log(x)
+
+let bob =20;
+console.log(bob)
+let boB =10;
+console.log(boB)
+
+// Datatypes
+
+/*
+Numbers
+Strings
+Booleans
+*/
+
+let temp=10; //Datatype number
+
+let temp1="10"; //Datatype string
+
+/*
+All numbers in javascript are floating point numbers
+*/
+
+/*
+All variables in javascript must begin with a
+letter or an underscore_. You can not a reserved word
+as a variable word.
+*/
+
+//Arithmatic Operaters
+
+//Addition
+
+console.log(5+2);
+
+console.log("abc+def");
+
+//Substraction
+
+console.log(5-2);
+
+//multiplication
+
+console.log(5*2);
+
+//division
+
+console.log(5/2);
+
+//exponent
+
+console.log(3**4);
+
+//increment
+
+let h=11;
+
+console.log(h++);
+
+console.log(h);
+
+let k =10;
+
+console.log(++k);
+
+//decerment operator
+
+let c = 5;
+
+console.log(c--);
+
+console.log(c);
+
+console.log(--c);
+
+//comparison operators
+
+//equlas ==
+
+console.log(5 == 3);
+
+console.log(4 == 4);
+
+// not equals !=
+
+console.log(5!=4);
+
+console.log(4!=4);
+
+// greater than >
+
+console.log(5>4);
+
+console.log(4>5);
+
+// less than <
+
+console.log(5<4);
+
+console.log(4<5);
+
+//greater than or less than >=
+
+console.log(5>=4);
+
+console.log(4>=5);
+
+// less than or equal to <=
+
+console.log(5<=4);
+
+console.log(4<=5);
